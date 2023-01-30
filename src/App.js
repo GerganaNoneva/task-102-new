0f837e08da332ccbf66b5f5936bc3a968d2007c4
GerganaNoneva <gergananoneva@gmail.com>
2023-01-30 18:03:12 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
 import "./App.css";
 
+import React from 'react';
+//import { motion } from 'framer-motion';
+import {motion} from "framer-motion/dist/framer-motion"
+
+function App() {
+  return (
+    <div className="App">
+    <motion.form
+      initial={{ x: '-100%' }}
+      animate={{ x: '50%' }}
+      style={{ position: 'absolute', top: '50%', transform: 'translateY(-50%)' }}
+    >
+      {/* Your form fields go here */
+      <form>
+        <h1>Create An Account</h1>
+        <label>
+          Email:
+          <input name="email" type="email" required />
+        </label>
+        <label>
+          Password:
+          <input name="password" type="password" required />
+        </label>
+        <button>Sign Up!</button>
+      </form>
+      }
+    </motion.form>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+/*import Form from "./Form";
+
 function App() {
   return (
     <div className="App">
@@ -15,8 +54,13 @@ function App() {
         </label>
         <button>Sign Up!</button>
       </form>
+      <Form/>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+*/
